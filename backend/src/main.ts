d64d55b3a15b74a4c54911832852ab88343182a7
Igor Slivca <i.slivca@medialis.tech>
2025-03-26 22:06:16 +0100

    funzione per il shutdown del server in maniera graziosa ☺

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,27 @@
 import { buildServer } from './utils/server'
 
+const gracefulShutdown = async ({
+  app,
+}: {
+  app: Awaited<ReturnType<typeof buildServer>>
+}) => {
+  await app.close()
+}
+
 const main = async () => {
   const app = await buildServer()
 
   app.listen({
     port: 3000,
   })
+
+  const signals = ['SIGINT', 'SIGTERM']
+
+  for (const signal of signals) {
+    process.on(signal, () => {
+      gracefulShutdown({ app })
+    })
+  }
 }
 
 main()
