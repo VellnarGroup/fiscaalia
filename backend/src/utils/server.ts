5183412ba5324dffdb4b3bcb84286f7f3adf6bfb
Igor Slivca <i.slivca@medialis.tech>
2025-03-27 20:26:57 +0100

    registra le routes dei ruoli e utenti

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,16 +1,64 @@
 import fastify from 'fastify'
-import { logger } from './logger'
+import guard from 'fastify-guard'
 import { databaseRoutes } from '../modules/databases/databases.routes'
+import { usersRoutes } from '../modules/users/users.routes'
+import { roleRoutes } from '../modules/roles/roles.routes'
+import jwt from 'jsonwebtoken'
+
+type User = {
+  id: string
+  databaseId: string
+  scopes: Array<string>
+}
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    user: User | null
+  }
+}
 
 export const buildServer = async () => {
   const app = fastify({
-    logger: logger,
+    logger: {
+      level: 'debug',
+      transport: {
+        target: 'pino-pretty',
+      },
+    },
+  })
+
+  app.decorateRequest('user', null)
+
+  app.addHook('onRequest', async (request, reply) => {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+      return
+    }
+
+    try {
+      const token = authHeader.replace('Bearer ', '')
+      const decoded = jwt.verify(token, 'secret') as User
+
+      console.log('user', decoded)
+
+      request.user = decoded
+    } catch (e) {}
   })
 
   // Fastify Plugins
+  app.register(guard, {
+    requestProperty: 'user',
+    scopeProperty: 'scopes',
+    errorHandler: (result, request, reply) => {
+      return reply.send('you can not do that')
+    },
+  })
 
   // Fastify Routes
   app.register(databaseRoutes, { prefix: '/api/databases' })
+  app.register(usersRoutes, { prefix: '/api/users' })
+  app.register(roleRoutes, { prefix: '/api/roles' })
 
   return app
 }
